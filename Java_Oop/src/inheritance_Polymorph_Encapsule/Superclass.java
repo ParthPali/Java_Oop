c1bf22faef9e19f43a618ef3f4d0ec29db557979
ParthPali <54657955+ParthPali@users.noreply.github.com>
2019-12-13 17:56:51 -0800

    Update Superclass.java

--- Java_Oop/src/inheritance_Polymorph_Encapsule/Superclass.java
+++ Java_Oop/src/inheritance_Polymorph_Encapsule/Superclass.java
@@ -4,10 +4,18 @@ public class Superclass {
 	
 	int accountNumber;
 	int amount;
+	int budget;
 	
 	public Superclass(int accountNumber, int amount, int budget) {
 		this.accountNumber = accountNumber;
 		this.amount = amount;
+		this.budget = budget;
+	}
+	
+	public Superclass(int accountNumber, int amount){
+		this.accountNumber = accountNumber;
+		this.amount = amount;
+		budget = 0;
 	}
 
 	public int getAmount() {
