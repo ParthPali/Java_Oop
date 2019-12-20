e42fed588ecbb8b16b63518420cc1b73d5872ccb
ParthPali <54657955+ParthPali@users.noreply.github.com>
2019-12-20 15:38:54 -0800

    Update Bank1.java

--- Java_Oop/src/inheritance_Polymorph_Encapsule/Bank1.java
+++ Java_Oop/src/inheritance_Polymorph_Encapsule/Bank1.java
@@ -2,7 +2,7 @@ package inheritance_Polymorph_Encapsule;
 
 public class Bank1 extends Superclass {
 	
-	int budget;
+	private int budget;
 	
 	public Bank1(int accountNumber, int amount, int budget) {
 		super(accountNumber, amount, budget);
